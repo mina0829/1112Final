7bb09db50c8c7d3467817c567d328eca7daa3745
mina0829 <116851886+mina0829@users.noreply.github.com>
2023-04-30 09:41:08 +0800

    add notes

--- Final/src/Main.java
+++ Final/src/Main.java
@@ -2,6 +2,7 @@ import javax.swing.JFrame;
 public class Main {
 
 	public static void main(String[] args) {
+		//這邊調整成可以連資料庫的格式
 		HomePage frame = new HomePage();//打開主介面
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.setVisible(true);
