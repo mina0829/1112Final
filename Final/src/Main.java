a9bee484607588ae45008e96494778654edd6323
mina0829 <116851886+mina0829@users.noreply.github.com>
2023-04-30 09:57:40 +0800

    re-upload Main class

---
+++ Final/src/Main.java
@@ -0,0 +1,11 @@
+import javax.swing.JFrame;
+
+public class Main {
+
+	public static void main(String[] args) {
+		HomePage frame = new HomePage();
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setVisible(true);
+	}
+
+}
