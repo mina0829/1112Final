ac537cbec2e76414ead396c76423bcf8ea9cbc90
mina0829 <116851886+mina0829@users.noreply.github.com>
2023-04-30 09:58:56 +0800

    連結資料庫

--- Final/src/Main.java
+++ Final/src/Main.java
@@ -1,11 +1,27 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 import javax.swing.JFrame;
 
 public class Main {
 
 	public static void main(String[] args) {
-		HomePage frame = new HomePage();
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		frame.setVisible(true);
+		String server = "jdbc:mysql://140.119.19.73:3315/";
+		String database = "111306012";
+		String url = server + database + "?useSSL=false";
+		String username = "111306012"; 
+		String password = "25cah";
+	
+		try {
+			Connection conn = DriverManager.getConnection(url, username, password);
+			System.out.println("DB Connectd");
+			HomePage frame = new HomePage();
+			frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+			frame.setVisible(true);
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		
 	}
 
 }
