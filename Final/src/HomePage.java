26fa548e63a8ef490a504f06311691c7f1f7caee
mina0829 <116851886+mina0829@users.noreply.github.com>
2023-05-29 17:55:03 +0800

    Update HomePage.java

--- Final/src/HomePage.java
+++ Final/src/HomePage.java
@@ -2,7 +2,11 @@ import java.awt.*;
 import javax.swing.*;
 import java.awt.event.*;
 import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
 import java.sql.*;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 //輸入內容皆從資料庫中讀取！
 
 public class HomePage extends JFrame{
@@ -16,7 +20,7 @@ public class HomePage extends JFrame{
 	private JTextArea postsArea;
 	
 	private EditPage editPage;
-	private ArrayList<JPanel>panels;
+	private Date date = new Date();
 	private boolean sucess;
 	Connection conn;
 	Statement stat;
@@ -28,8 +32,10 @@ public class HomePage extends JFrame{
 		this.setTitle("惜食平臺");
 		this.setSize(this.FRAME_WIDTH, this.FRAME_HEIGHT);
 		
-		creatButton();
 		creatTextArea();
+		creatButton();
+		creatLabel();
+		creatTextField();
 		creatPanel();
 	}
 	
@@ -49,12 +55,15 @@ public class HomePage extends JFrame{
 					sucess = stat.execute(query);
 					if(sucess) {
 						ResultSet result = stat.getResultSet();
-						postsArea.setText(showResultSet(result));
+						System.out.println("check");
+						postsArea.repaint();
+						postsArea.setText(showResultSet(result, stat));
 					}
 				} catch (SQLException e) {
 					e.printStackTrace();
+				} catch (ParseException e) {
+					System.out.println("error");
 				}
-				//這邊要修改，改成只update結果顯示區，在顯示區做時間到了就不顯示的功能
 			}
 		});
 		
@@ -62,6 +71,7 @@ public class HomePage extends JFrame{
 			public void actionPerformed(ActionEvent event) {
 				try {
 					editPage = new EditPage(conn);
+					editPage.setVisible(true);
 				}catch(SQLException e) {
 					e.printStackTrace();
 				}
@@ -75,20 +85,13 @@ public class HomePage extends JFrame{
 		});
 	}
 	
-	public void creatTextArea() {
-		postsArea = new JTextArea();
-		postsArea.setText("");
-		postsArea.setEditable(false);
-	}
-	
 	public void creatTextField() {
 		keyWordF = new JTextField(5);
 	}
 	
-		public void creatTextArea() {
-		postsArea = new JTextArea(800, 400);
+	public void creatTextArea() {
+		postsArea = new JTextArea(40, 70);
 		postsArea.setText("");
-		postsArea.setEditable(false);
 	}
 	
 	public void creatPanel() {
@@ -104,7 +107,7 @@ public class HomePage extends JFrame{
 		postsP = new JPanel();
 		postsP.add(postsArea);
 		
-		overallP = new JPanel(new GridLayout(2, 1));
+		overallP = new JPanel();
 		overallP.add(buttonsP);
 		overallP.add(keyWordP);
 		overallP.add(postsP);
@@ -112,12 +115,13 @@ public class HomePage extends JFrame{
 	}
 	
 	public void keywordSearch(){
-		 String keyword = keyWordF.getText();
+		String keyword = keyWordF.getText();
 		 String[] columns = {
 				 "title", "location", 
 				 "itemName1", "itemName2", "itemName3",  "itemName3", "itenName4",
 				 "note", "tag1", "tag2", "tag3", "tag4", "tag5"
 		 };
+		
 		 for(String column : columns) {
 			 String query = "SELECT * FROM posts WHERE "+column+" LIKE " + keyword;
 			 try {
@@ -125,20 +129,26 @@ public class HomePage extends JFrame{
 		            statement.setString(1, "%" + keyword + "%");
 		            ResultSet resultSet = statement.executeQuery();
 	
-		            // 處理搜索结果
 		            while (resultSet.next()) {
-		                // 處理每一行數據
-		                String result = resultSet.getString(column);
-		                System.out.println(result);
+		               postsArea.setText(showResultSet(resultSet, stat));
 		            }
-		        } catch (SQLException e) {
+		    
+		       } catch (SQLException e) {
 		            e.printStackTrace();
-		        }
+		       } catch (ParseException e) {
+					System.out.println("error");
+		       }
 	   }
 	 }
 	 //設定button, textField
+	
+	public boolean checkTime() { 
+		boolean finish = false;
+		 String query = "SELECT * FROM posts WHERE endTime";
+		return finish;
+	}
 
-	public static String showResultSet(ResultSet result) throws SQLException {
+	public static String showResultSet(ResultSet result, Statement stat) throws SQLException, ParseException{
 		ResultSetMetaData metaData = result.getMetaData();
 		int columnCount = metaData.getColumnCount();
 		String output = "";
@@ -150,23 +160,28 @@ public class HomePage extends JFrame{
 			String location = result.getString(2);
 			
 			for(int i = 1; i < 5; i++) {
-				String itemName = "itemName" + (i);
-				if(result.getString(itemName) != null) {
-					items.add(String.format("%s，剩餘%d份", result.getString(itemName), result.getInt((i+1)*2)));
+				if(result.getInt((i+1)*2) != 0) {
+						items.add(String.format("%s，剩餘%d份", result.getString((i*2)+1), result.getInt((i+1)*2)));
 				}
-			}
+			}//數字為0時不顯示
 			
-			String note = result.getString(9);
+			String note = result.getString(11);
+			
 			for(int i = 1; i < 6; i++) {
-				String tag = "tag" + i;
-				if(result.getString(tag) != null){
-					tags.add(String.format("#%s", result.getString(tag)));
-				}
+				tags.add(result.getString(i+11) + " ");
 			}
 			
-			//圖片顯示
+			String endTime = result.getString(18);
 			
-			String endTime = result.getString(19);
+			/*Calendar calendar = Calendar.getInstance();
+			Date date = new Date();
+			calendar.setTime(date);
+			
+			Date endDate = new SimpleDateFormat("yyyy-MM-dd hh:mm:ss").parse("2023-"+endTime);
+			if(calendar.getTime().compareTo(endDate) > 0) {
+				String query = "UPDATE `posts` SET state = '1' WHERE title = '" + title + "';";
+				stat.execute(query);
+			}*/
 			
 			output += String.format("%s\n%s．%s\n", title, endTime, location);
 			
@@ -176,10 +191,14 @@ public class HomePage extends JFrame{
 			
 			output += String.format("備註：%s\n", note);
 			
+			output += "#標籤：";
 			for(String tag: tags) {
 				output += tag;
 			}
-			output += "\n";
+			output +="\n" + "-".repeat(40) + "\n";
+			
+			items.clear();
+			tags.clear();
 		}
 		return output;
 	}
