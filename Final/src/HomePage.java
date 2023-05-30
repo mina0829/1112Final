6bc3fac1c38dc54163c63902a541d049e15e7819
mina0829 <116851886+mina0829@users.noreply.github.com>
2023-05-30 11:53:24 +0800

    Update HomePage.java

--- Final/src/HomePage.java
+++ Final/src/HomePage.java
@@ -18,12 +18,14 @@ public class HomePage extends JFrame{
 	private JPanel buttonsP, postsP, overallP, keyWordP;
 	private JTextField keyWordF;
 	private JTextArea postsArea;
+	private JScrollPane scrollPane;
 	
 	private EditPage editPage;
 	private Date date = new Date();
 	private boolean sucess;
 	Connection conn;
 	Statement stat;
+	ResultSet resultSet;
 	
 	public HomePage (Connection conn) throws SQLException{
 		this.conn = conn;
@@ -37,6 +39,8 @@ public class HomePage extends JFrame{
 		creatLabel();
 		creatTextField();
 		creatPanel();
+		checkTime();
+		updateTextArea();
 	}
 	
 	public void creatLabel() {
@@ -50,20 +54,8 @@ public class HomePage extends JFrame{
 		
 		updateButton.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent event) {
-				String query = "SELECT * FROM `posts` WHERE state = 1";
-				try{
-					sucess = stat.execute(query);
-					if(sucess) {
-						ResultSet result = stat.getResultSet();
-						System.out.println("check");
-						postsArea.repaint();
-						postsArea.setText(showResultSet(result, stat));
-					}
-				} catch (SQLException e) {
-					e.printStackTrace();
-				} catch (ParseException e) {
-					System.out.println("error");
-				}
+				checkTime();
+				updateTextArea();
 			}
 		});
 		
@@ -85,67 +77,132 @@ public class HomePage extends JFrame{
 		});
 	}
 	
+	public void updateTextArea() {
+		String query = "SELECT * FROM `posts` WHERE state = 1";
+		try{
+			sucess = stat.execute(query);
+			if(sucess) {
+				ResultSet result = stat.getResultSet();
+				System.out.println("check");
+				postsArea.repaint();
+				postsArea.setText(showResultSet(result, stat));
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} catch (ParseException e) {
+			System.out.println("error");
+		}
+	}
+	
 	public void creatTextField() {
 		keyWordF = new JTextField(5);
 	}
 	
 	public void creatTextArea() {
-		postsArea = new JTextArea(40, 70);
-		postsArea.setText("");
+	    postsArea = new JTextArea(30, 75);
+	    postsArea.setText("");
+
+	    scrollPane = new JScrollPane(postsArea);
+	    scrollPane.setPreferredSize(new Dimension(650, 450)); // Adjust the preferred size of the scroll pane
+
+	    Font font = new Font("Microsoft JhengHei", Font.PLAIN, 10);
+	    postsArea.setFont(font);
+
+	    // Adjust the font size without affecting the JTextArea's size
+	    Font currentFont = postsArea.getFont();
+	    Font newFont = currentFont.deriveFont(20f); // Set font size to 20
+	    postsArea.setFont(newFont);
 	}
 	
 	public void creatPanel() {
-		buttonsP = new JPanel();
-		buttonsP.add(updateButton);
-		buttonsP.add(addPostButton);
-		
-		keyWordP = new JPanel();
-		keyWordP.add(keyWordL);
-		keyWordP.add(keyWordF);
-		keyWordP.add(searchButton);
-		
-		postsP = new JPanel();
-		postsP.add(postsArea);
-		
-		overallP = new JPanel();
-		overallP.add(buttonsP);
-		overallP.add(keyWordP);
-		overallP.add(postsP);
-		this.add(overallP);
+	    buttonsP = new JPanel();
+	    buttonsP.add(updateButton);
+	    buttonsP.add(addPostButton);
+
+	    keyWordP = new JPanel();
+	    keyWordP.add(keyWordL);
+	    keyWordP.add(keyWordF);
+	    keyWordP.add(searchButton);
+
+	    postsP = new JPanel();
+	    postsP.add(scrollPane); 
+
+	    overallP = new JPanel();
+	    overallP.add(buttonsP);
+	    overallP.add(keyWordP);
+	    overallP.add(postsP);
+	    this.add(overallP);
 	}
 	
-	public void keywordSearch(){
-		String keyword = keyWordF.getText();
-		 String[] columns = {
-				 "title", "location", 
-				 "itemName1", "itemName2", "itemName3",  "itemName3", "itenName4",
-				 "note", "tag1", "tag2", "tag3", "tag4", "tag5"
-		 };
-		
-		 for(String column : columns) {
-			 String query = "SELECT * FROM posts WHERE "+column+" LIKE " + keyword;
-			 try {
-		            PreparedStatement statement = conn.prepareStatement(query);
-		            statement.setString(1, "%" + keyword + "%");
-		            ResultSet resultSet = statement.executeQuery();
-	
-		            while (resultSet.next()) {
-		               postsArea.setText(showResultSet(resultSet, stat));
-		            }
-		    
-		       } catch (SQLException e) {
-		            e.printStackTrace();
-		       } catch (ParseException e) {
-					System.out.println("error");
-		       }
-	   }
+	public void keywordSearch() {
+	    String keyword = keyWordF.getText();
+	    String[] columns = {
+	        "title", "location",
+	        "itemName1", "itemName2", "itemName3", "itemName4",
+	        "note", "tag1", "tag2", "tag3", "tag4", "tag5"
+	    };
+
+	    StringBuilder queryBuilder = new StringBuilder();
+	    queryBuilder.append("SELECT * FROM posts WHERE state = 1 AND (");
+	    for (int i = 0; i < columns.length; i++) {
+	        queryBuilder.append(columns[i]).append(" LIKE ?");
+	        if (i < columns.length - 1) {
+	            queryBuilder.append(" OR ");
+	        }
+	    }
+	    queryBuilder.append(")");
+
+	    String query = queryBuilder.toString();
+	    try {
+	        PreparedStatement statement = conn.prepareStatement(query);
+	        for (int i = 1; i <= columns.length; i++) {
+	            statement.setString(i, "%" + keyword + "%");
+	        }
+	        System.out.println(query);
+	        ResultSet resultSet = statement.executeQuery();
+
+	        while (resultSet.next()) {
+	            postsArea.repaint();
+	            postsArea.setText(showResultSet(resultSet, stat));
+	        }
+	    } catch (SQLException e) {
+	        e.printStackTrace();
+	    } catch (ParseException e) {
+	        e.printStackTrace();
+	    }
 	 }
 	 //設定button, textField
 	
-	public boolean checkTime() { 
-		boolean finish = false;
-		 String query = "SELECT * FROM posts WHERE endTime";
-		return finish;
+	public boolean checkTime() {
+	    boolean finish = false;
+	    String query = "SELECT * FROM posts WHERE state = 1";
+	    try {
+	        ResultSet resultSet = stat.executeQuery(query);
+	        while (resultSet.next()) {
+	            String title = resultSet.getString(1);
+	            String endTimeString = "2023-" + resultSet.getString(18); // 從資料庫中取得 endTime 的字串形式
+	            
+	            SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm");
+	            Date endTime = dateFormat.parse(endTimeString); // 將 endTime 字串轉換為日期物件
+	            
+	            Date currentDate = new Date();
+	            //Calendar currentCalendar = Calendar.getInstance(); // 取得目前時間的 Calendar 物件
+
+	            if (currentDate.after(endTime)) {
+	            	System.out.println(currentDate.toString() + " after " + endTime.toString());
+	                String updateQuery = "UPDATE posts SET state = '0' WHERE title = ?";
+	                PreparedStatement updateStatement = conn.prepareStatement(updateQuery);
+	                updateStatement.setString(1, title);
+	                updateStatement.executeUpdate();
+	            }
+	        }
+	        finish = true;
+	    } catch (SQLException e) {
+	        e.printStackTrace();
+	    } catch (ParseException e) {
+	        e.printStackTrace();
+	    }
+	    return finish;
 	}
 
 	public static String showResultSet(ResultSet result, Statement stat) throws SQLException, ParseException{
@@ -172,16 +229,6 @@ public class HomePage extends JFrame{
 			}
 			
 			String endTime = result.getString(18);
-			
-			/*Calendar calendar = Calendar.getInstance();
-			Date date = new Date();
-			calendar.setTime(date);
-			
-			Date endDate = new SimpleDateFormat("yyyy-MM-dd hh:mm:ss").parse("2023-"+endTime);
-			if(calendar.getTime().compareTo(endDate) > 0) {
-				String query = "UPDATE `posts` SET state = '1' WHERE title = '" + title + "';";
-				stat.execute(query);
-			}*/
 			
 			output += String.format("%s\n%s．%s\n", title, endTime, location);
 			
